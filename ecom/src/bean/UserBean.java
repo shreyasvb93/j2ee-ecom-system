0671916814c4acb95ab90fbb2d881836424b3aee
shreyasvb93 <shreyasvb93@gmail.com>
2017-04-01 21:21:24 -0400

    UserBase updated - Test

--- ecom/src/bean/UserBean.java
+++ ecom/src/bean/UserBean.java
@@ -1,10 +1,39 @@
 package bean;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserBean {
 
 	private int userID;
 	private String uName;
 	private String pWord;
+	private String lastlogin;
 	
+	protected String getLastlogin() {
+		return lastlogin;
+	}
+	protected void setLastlogin(String lastlogin) {
+		this.lastlogin = lastlogin;
+	}
+	protected int getUserID() {
+		return userID;
+	}
+	protected void setUserID(int userID) {
+		this.userID = userID;
+	}
+	protected String getuName() {
+		return uName;
+	}
+	protected void setuName(String uName) {
+		this.uName = uName;
+	}
+	protected String getpWord() {
+		return pWord;
+	}
+	protected void setpWord(String pWord) {
+		this.pWord = pWord;
+	}
 
+	
 }
